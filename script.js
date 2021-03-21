c34d6ef20363f352ae8dbe985d4f95cfe9656708
piwiit <bouillaux@hotmail.fr>
2021-03-21 14:29:06 +0100

    add random color

--- script.js
+++ script.js
@@ -1,30 +1,49 @@
-const gridContainer = document.getElementById("grid-container");
-
-window.addEventListener("load", setDefaultGrid);
+const gridContainer = document.getElementById('grid-container');
+const initButton = document.querySelector('#init-button');
+console.log(initButton);
 
 //create a default grid sized 16x16
 let setDefaultGrid = () => {
-	setGridSize(16);
-	fillGrid(16);
+  setGridSize(16);
+  fillGrid(16);
 };
 
+//load setDefaulgrid first
+window.addEventListener('load', setDefaultGrid);
+
 let setGridSize = (size) => {
-	gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 };
 
 let fillGrid = (size) => {
-	for (let i = 0; i < size * size; i++) {
-		let gridElement = document.createElement("div");
-		//gridElement.addEventListener("mouseover", getRandomColor());
-		gridContainer.appendChild(gridElement).classList = "grid-element";
-	}
+  for (let i = 0; i < size * size; i++) {
+    let gridElement = document.createElement('div');
+    gridContainer.appendChild(gridElement).classList = 'grid-element';
+    gridElement.addEventListener('mouseover', changeColor);
+  }
 };
 
-let getRandomColor = (e) => {
-	let letters = "0123456789ABCDEF";
-	let color = "#";
-	for (let i = 0; i < 6; i++) {
-		color += letters[Math.floor(Math.random() * 16)];
-	}
-	e.target.style.backgroundColor = color;
+//changing color
+let changeColor = (e) => {
+  e.target.style.background = getRandomColor();
 };
+
+//random color
+let getRandomColor = () => {
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+initButton.addEventListener('click', () => {
+  let size = Number(prompt('Enter size !'));
+  if (size === 0 || size === NaN) {
+    gridContainer.remove();
+  } else {
+    setGridSize(size);
+    fillGrid;
+  }
+});
