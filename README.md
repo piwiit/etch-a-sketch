ecbf115ed1964b124cd309cebf09dcdb3b5156b4
piwiit <bouillaux@hotmail.fr>
2021-03-29 20:49:54 +0200

    add eslint rules

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # etch-a-sketch
+
+Etch a sketch game created with HTML, CSS and JS.
+
+[Live demo](https://piwiit.github.io/etch-a-sketch/.)
--- script.js
+++ script.js
@@ -1,52 +1,58 @@
-const gridContainer = document.getElementById('grid-container');
-const initButton = document.querySelector('#init-button');
-console.log(initButton);
+const gridContainer = document.getElementById("grid-container");
+const initButton = document.querySelector("#init-button");
 
-//create a default grid sized 16x16
-let setDefaultGrid = () => {
-  setGridSize(16);
-  fillGrid(16);
-};
-
-//load setDefaulgrid first
-window.addEventListener('load', setDefaultGrid);
-
-let setGridSize = (size) => {
+const setGridSize = (size) => {
   gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 };
 
-let fillGrid = (size) => {
-  for (let i = 0; i < size * size; i++) {
-    let gridElement = document.createElement('div');
-    gridContainer.appendChild(gridElement).classList = 'grid-element';
-    gridElement.addEventListener('mouseover', changeColor);
-  }
-};
-
-//changing color
-let changeColor = (e) => {
-  e.target.style.background = getRandomColor();
-};
-
-//random color
-let getRandomColor = () => {
-  let letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
+// random color
+const getRandomColor = () => {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i += 1) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
 };
 
-let changeSize = () => {
-  let size = Number(prompt('Enter size !'));
-  if (size === 0 || size === NaN) {
-    alert('Enter a number of new grid');
-    changeSize();
-  } else {
-    setGridSize(size);
-    fillGrid(size);
+// changing color
+const changeColor = (e) => {
+  e.target.style.background = getRandomColor();
+};
+
+const fillGrid = (size) => {
+  for (let i = 0; i < size * size; i += 1) {
+    const gridElement = document.createElement("div");
+    gridContainer.appendChild(gridElement).classList = "grid-element";
+    gridElement.addEventListener("mouseover", changeColor);
   }
 };
 
-initButton.addEventListener('click', changeSize);
+// create a default grid sized 16x16
+const setDefaultGrid = () => {
+  setGridSize(16);
+  fillGrid(16);
+};
+
+// load setDefaulgrid first
+window.addEventListener("load", setDefaultGrid);
+
+// const customPrompt = alert("Enter size : ");
+
+// const changeSize = () => {
+//   const size = Number(customPrompt);
+//   if (size === 0 || isNaN(size)) {
+//     alert("Enter a number of new grid");
+//     changeSize();
+//   } else {
+//     setGridSize(size);
+//     fillGrid(size);
+//   }
+// };
+
+const changeSize = () => {
+  // eslint-disable-next-line no-alert
+  alert("Coming soon");
+};
+
+initButton.addEventListener("click", changeSize);
